fec741964c1125abccd00952925854beb94580e0
Mukamz <mukamatidennis@gmail.com>
2023-06-25 12:14:00 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * print without printf and puts
+ *main - print without printf and puts
  *
- * Return 1
+ * Return always (1)
  *
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
 
