b612a311fa67d5a4045fb320df98f534cdc31d86
Mukamz <mukamatidennis@gmail.com>
2023-06-25 12:16:26 -0700

    changes to write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  *main - print without printf and puts
  *
- * Return always (1)
+ *return always (1)
  *
  */
 int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
 
