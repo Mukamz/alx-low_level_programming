6b0b2290ef1f2292d6044d5bc8a2f072b7c5f928
Mukamz <mukamatidennis@gmail.com>
2023-09-08 02:25:11 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  *main - print without printf and puts
@@ -8,7 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char *message =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
+	write(2, message, 59);
 	return (1);
 }
-
