b5455f2e328cd275ed14e6c8742bdc9ebdbe1702
Mukamz <mukamatidennis@gmail.com>
2023-09-13 07:54:55 -0700

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    int i, j;
-    
-    for (i = 0; i <= 8; i++) {
-        for (j = i   return 0;
+/**
+ * main - Entry point
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+        int i, j;
+
+        for (int i = 0; i <= 8; i++)
+        {
+                        for (int j = i + 1; j <= 9; j++)
+                        {
+                                putchar('0' + i);
+                                putchar('0' + j);
+                                if (!(i == 8 && j == 9))
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        putchar (10);
+        return (0);
 }
