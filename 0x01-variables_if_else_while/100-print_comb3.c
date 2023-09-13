b5900af6a6dd6748af78fea67eeeb5e4c15088bb
Mukamz <mukamatidennis@gmail.com>
2023-09-13 08:19:22 -0700

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 
         for (int i = 0; i <= 8; i++)
         {
-                        for (int j = i + 1; j <= 9; j++)
+                        for (j = i + 1; j <= 9; j++)
                         {
                                 putchar('0' + i);
                                 putchar('0' + j);
